401395ce332d375a4bf09c6c3f2498ab4c7165e3
Zoran Musulin <zoran.musulin@gmail.com>
2019-04-15 22:06:27 +0200

    Adding products container

--- ecommerce/resources/views/welcome.blade.php
+++ ecommerce/resources/views/welcome.blade.php
@@ -34,8 +34,74 @@
                         <a href="#" class="button button-white">Button 1</a>
                         <a href="#" class="button button-white">Button 2</a>
                     </div><!-- end hero-copy-->
+                    <div class="hero image">
+                        <img src="img/macbook-pro-laravel.png" alt="hero image">
+                    </div>
                 </div>
-            </div>
+            </div><!-- end hero -->
         </header>
+        <div class="featured section">
+            <div class="container">
+                <h1 class="text-center">CSS Grid Example</h1>
+                <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae cumque iste doloremque quisquam! Illum pariatur dignissimos distinctio beatae, voluptatum dolore ut officia perspiciatis vitae dolor accusantium recusandae impedit atque id.</p>
+
+                    <div class="text-center button-container">
+                    <a href="#" class="button">Feature</a>
+                     <a href="#" class="button">On Sale</a> 
+                    </div>
+                     <div class="products text-center"></div>
+                        <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                       
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                         <div class="products">
+                            <a href=""><img src="img/macbook-pro.png" alt="product"></a>
+                            <a href=""><span class="product-name">MacBook Pro</span></a>
+                            <div class="product-price">$2499.99</div>
+                        </div>
+                    </div>   
+            </div><!-- end products -->
+
+            <div class="text-center button-container">
+                <a href="#" class="button">View more products</a>
+            </div>
+        </div> <!-- end container -->
+        </div> <!-- end featured-section -->
     </body>
 </html>
