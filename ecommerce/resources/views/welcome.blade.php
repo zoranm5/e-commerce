4c5b916797508667d0d8d3cc607d59e31c8007f7
zoranm5 <zoran.musulin@gmail.com>
2019-04-15 23:44:16 +0200

    Update welcome.blade.php

--- ecommerce/resources/views/welcome.blade.php
+++ ecommerce/resources/views/welcome.blade.php
@@ -103,5 +103,35 @@
             </div>
         </div> <!-- end container -->
         </div> <!-- end featured-section -->
+        <div class="blog-section">
+            <div class="container">
+                <h1 class="class">From our blog</h1>
+                <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+               <div class="blog-posts">
+                   <div class="blog-post">
+                       <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                       <a href="#"><h2 class="blog-title">Blog Posti title</h2></a>
+                       <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<div>
+                   </div>
+                   <div class="blog-post">
+                       <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                       <a href="#"><h2 class="blog-title">Blog Posti title</h2></a>
+                       <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<div>
+                   </div>
+                   <div class="blog-post">
+                       <a href="#"><img src="img/blog1.png" alt="blog image"></a>
+                       <a href="#"><h2 class="blog-title">Blog Posti title</h2></a>
+                       <div class="blog-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<div>
+                   </div>
+               </div> <!-- end blog-posts -->
+            </div><!-- end container -->
+        </div> <!--end blog-section-->
     </body>
 </html>
