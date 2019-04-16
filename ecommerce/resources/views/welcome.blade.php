50ff7fc382b9204c3db644be1b401619ba34a210
Zoran Musulin <zoran.musulin@gmail.com>
2019-04-16 13:15:59 +0200

    Adding fotter

--- ecommerce/resources/views/welcome.blade.php
+++ ecommerce/resources/views/welcome.blade.php
@@ -133,5 +133,18 @@
                </div> <!-- end blog-posts -->
             </div><!-- end container -->
         </div> <!--end blog-section-->
+
+        <fotter>
+            <div class="footer-content container">
+            <div class="mid with"><i class= "fa fa-heart"></i>by XXX XXXXX</div>
+            <ul>
+                <li>Follow Me</li>
+                <li><a href="#"><i class= "fa fa-globe"></i></a></li>
+                <li><a href="#"><i class= "fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class= "fa fa-github"></i></a></li>
+                <li><a href="#"><i class= "fa fa-twitter"></i></a></li>   
+            </ul>
+        </div><!-- end footer-content -->
+        </fotter>
     </body>
 </html>
